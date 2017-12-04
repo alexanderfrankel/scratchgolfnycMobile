28188827cd3f1555bf145d491d1d05796e241b40
Alexander Frankel <alexxander.frankel@gmail.com>
2017-12-04 15:19:25 -0500

    Refactor index.js

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer from './app/reducers';
+
+const loggerMiddleware = createLogger({predicate: (getState, action) => __DEV__});
+
+function configureStore(initialState) {
+  const enhancer = compose(
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    ),
+  );
+  return createStore(reducer, initialState, enhancer);
+}
+
+const store = configureStore({});
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+const App = () => (
+  <Provider store={store}>
+    <Home />
+  </Provider>
+);
 
 AppRegistry.registerComponent('scratchgolfnycMobile', () => App);
