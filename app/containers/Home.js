d0ac1aa01583b20e30b10cfae02caa2911c52ea4
Alexander Frankel <alexxander.frankel@gmail.com>
2017-12-05 18:44:09 -0500

    Add styling to home container

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -12,26 +12,81 @@ const {
 } = ReactNative
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searching: false, searchInput: '' }
+  }
 
   searchGames() {
-    this.props.fetchGames()
+    this.setState({searching: true});
+    this.props.fetchGames(this.state.searchInput).then( () => {
+      this.setState({searching: false});
+    });
+  }
+
+  games() {
+    return Object.keys(this.props.searchedGames).map((key) => this.props.searchedGames[key] );
   }
 
   render() {
     return (
-      <View style={{marginTop: 20}}>
-        <View>
-          <TouchableHighlight onPress={() => this.searchGames()}>
+      <View style={styles.scene}>
+        <View style={styles.searchSection}>
+          <TextInput style={styles.searchInput}
+            returnKeyType='search'
+            placeholder='Search'
+            onChangeText={ (searchInput) => this.setState({searchInput}) }
+            value={this.state.searchInput}
+          />
+          <TouchableHighlight onPress={() => this.searchGames()} style={styles.searchButton}>
             <Text>Search Games</Text>
           </TouchableHighlight>
         </View>
-
-        <ScrollView>
+        <ScrollView style={styles.scrollSection}>
+        {!this.state.searching && this.games().map((game) => {
+          return <View key={game.id}>
+            <Image source={{}} style={styles.resultImage} />
+            <Text style={styles.resultText}>{game.course_name}</Text>
+            <Text style={styles.resultText}>{game.date}</Text>
+          </View>
+        })}
+        { this.state.searching ? <Text>Seaching...</Text> : null }
         </ScrollView>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+    marginTop: 20,
+  },
+  searchSection: {
+    height: 30,
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    padding: 5,
+    flexDirection: 'row',
+  },
+  searchInput: {
+    flex: 0.7,
+  },
+  searchButton: {
+    flex: 0.3,
+  },
+  scrollSection: {
+    flex: 0.8,
+  },
+  resultImage: {
+    height: 150,
+  },
+  resultText: {
+    backgroundColor: '#000',
+    color: '#FFF',
+    height: 20,
+  },
+});
 
 const mapStateToProps = (state) => {
   return {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import reducer from './app/reducers';
