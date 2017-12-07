18ebb5203960226939dcfa1c81d89ea2ddd8f80d
Alexander Frankel <alexxander.frankel@gmail.com>
2017-12-06 23:42:46 -0500

    Extract store logic from entry point

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { navigationReducer } from './navigation';
 import * as gamesReducer from './games';
 
 export default combineReducers(Object.assign({},
--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import reducer from './app/reducers';
+import configureStore from './app/store/configureStore';
 import AppContainer from './app/containers/AppContainer';
 
-const loggerMiddleware = createLogger({predicate: (getState, action) => __DEV__});
-
-const configureStore = (initialState) => {
-  const enhancer = compose(
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
-    ),
-  );
-  return createStore(reducer, initialState, enhancer);
-}
-
-const store = configureStore({});
+const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
