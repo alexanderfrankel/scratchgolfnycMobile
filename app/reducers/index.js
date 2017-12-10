23413d00b3e2804548e7ad8814cc295197a2952c
Alexander Frankel <alexxander.frankel@gmail.com>
2017-12-10 13:11:36 -0500

    Fix app reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { navigationReducer } from './navigation';
 import * as gamesReducer from './games';
 
 export default combineReducers(Object.assign({},
