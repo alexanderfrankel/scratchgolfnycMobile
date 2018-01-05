952b612ae7a372f6100192d4a22df5e6568609b2
Alexander Frankel <alexxander.frankel@gmail.com>
2018-01-05 16:41:35 -0500

    Add image on login screen

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -6,16 +6,23 @@ import ReactNative from 'react-native';
 const {
   View,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  Image,
+  StyleSheet,
 } = ReactNative
 
 class Login extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <TouchableHighlight onPress={ () => { this.props.login() } }>
-          <Text>Login</Text>
-        </TouchableHighlight>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image source={require('../../assets/images/loginScreen.jpg')} style={styles.backgroundImage} />
+        </View>
+        <View style={styles.loginContainer}>
+          <TouchableHighlight onPress={ () => { this.props.login() } }>
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     )
   };
@@ -24,6 +31,36 @@ class Login extends Component {
 mapDispatchToProps = (dispatch) => {
   return bindActionCreators(ActionCreators, dispatch);
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  imageContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+
+  backgroundImage: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+
+  loginContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  loginText: {
+    fontSize: 60,
+    color: 'white',
+  },
+})
 
 export default connect(() => { return {} }, mapDispatchToProps)(Login);
 
