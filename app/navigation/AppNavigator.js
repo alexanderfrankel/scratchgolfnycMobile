31691471df3ee1fda011f2881479beb694d4561f
Alexander Frankel <alexxander.frankel@gmail.com>
2018-01-05 14:35:16 -0500

    Add tab navigator to main screen

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,16 +1,33 @@
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import Login from '../screens/Login';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
 
-export default StackNavigator({
-  Login: {
-    screen: Login
-  },
+const Tabs = TabNavigator({
   Home: {
-    screen: Home
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      title: "Home",
+    },
   },
   Profile: {
-    screen: Profile
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: "Profile",
+      title: "Profile",
+    },
+  },
+});
+
+export default StackNavigator({
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      header: null,
+    }
+  },
+  Main: {
+    screen: Tabs,
   },
 });
--- app/reducers/nav.js
+++ app/reducers/nav.js
@@ -3,38 +3,15 @@ import AppNavigator from '../navigation/AppNavigator';
 import * as types from '../actions/types';
 
 const ActionForLoggedOut = AppNavigator.router.getActionForPathAndParams('Login');
-const ActionForLoggedIn = AppNavigator.router.getActionForPathAndParams('Home');
+const ActionForLoggedIn = AppNavigator.router.getActionForPathAndParams('Main');
 
 const stateForLoggedOut = AppNavigator.router.getStateForAction(ActionForLoggedOut);
-const stateForLoggedIn = AppNavigator.router.getStateForAction(ActionForLoggedIn);
+const stateForLoggedIn = AppNavigator.router.getStateForAction(ActionForLoggedIn, stateForLoggedOut);
 
 const initialState = { stateForLoggedOut, stateForLoggedIn }
 
 export const nav = (state = initialState, action) => {
   switch (action.type) {
-    case "@@redux/INIT":
-      return {
-        ...state,
-        stateForLoggedIn: AppNavigator.router.getStateForAction(ActionForLoggedIn, stateForLoggedOut)
-      };
-
-    case types.LOGIN:
-      return {
-        ...state,
-        stateForLoggedIn: AppNavigator.router.getStateForAction(ActionForLoggedIn, stateForLoggedOut)
-      }
-
-    case types.LOGOUT:
-      return {
-        ...state,
-        stateForLoggedOut: AppNavigator.router.getStateForAction(
-          NavigationActions.reset({
-            index: 0,
-            actions: [NavigationActions.navigate({ routeName: "Login" })]
-          })
-        )
-      };
-
     default:
       return {
         ...state,
