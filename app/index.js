d81c23122767bc626d4ad92b3afa5d1faf5aa215
Alexander Frankel <alexxander.frankel@gmail.com>
2018-01-12 12:18:05 -0500

    Clean up store

--- app/index.js
+++ app/index.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
-import configureStore from './store/configureStore';
+
+import { store, persistor } from './store';
+import * as session from './services/session';
 import LaunchScreen from './screens/LaunchScreen';
 import AppNavigation from './navigation'
 
-const { persistor, store } = configureStore();
+const onBeforeLift = () => {
+  session.refreshToken();
+};
 
 export default () => {
   return (
     <Provider store={store}>
       <PersistGate
         loading={<LaunchScreen />}
+        onBeforeLift={onBeforeLift}
         persistor={persistor}>
         <AppNavigation />
       </PersistGate>
